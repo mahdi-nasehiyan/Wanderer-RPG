f9e92cbc04b171bf2c547b9f878ac32c4a10d63a
Mahdi Nasehiyan <im.insightman@gmail.com>
2024-06-03 14:33:06 +0430

    Refactored the wanderer module

--- wanderer/Hero.py
+++ wanderer/Hero.py
@@ -1,10 +1,19 @@
-class Hero:
+from wanderer.Character import *
 
-    def __init__(self):
-        self.x = 0
-        self.y = 0
-        self.img = "hero_down"
 
-    def move(self, x = 0, y = 0):
-        self.x += x
-        self.y += y
+class Hero(Character):
+    def __init__(self, level=1):
+        super(Hero, self).__init__("hero_down", level, "Hero")
+        # setting the initial stats of the hero
+        self.stats.set_stats(
+            20 + (3 * roll_dice()),
+            2 * roll_dice(),
+            5 + roll_dice(),
+            True
+        )
+
+    def level_up(self):
+        self.level += 1
+        self.stats.max_health_point += roll_dice()
+        self.stats.strike_point += roll_dice()
+        self.stats.defend_point += roll_dice()
---
+++ wanderer/__init__.py
@@ -0,0 +1,7 @@
+from wanderer.Boss import Boss
+from wanderer.Character import Character
+from wanderer.CharacterStats import CharacterStats
+from wanderer.Game import Game
+from wanderer.Hero import Hero
+from wanderer.Map import Map
+from wanderer.Monster import Monster
